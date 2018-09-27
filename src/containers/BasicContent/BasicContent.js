7ea625e1bdf6d63e37e0538a61ed77a180be7b4d
Max Barry <max@mxbry.com>
2018-09-28 00:23:47 +0100

    Added a placeholder for content pages

---
+++ src/containers/BasicContent/BasicContent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const tmpStyles = {
+    minHeight: '100vh',
+    minWidth: '100vw',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontFamily: 'Helvetica Neue, Arial',
+    fontWeight: 300,
+    fontSize: 56
+};
+
+class BasicContent extends Component {
+    render() {
+        return <div style={tmpStyles}>Content pages TBD</div>;
+    }
+}
+
+export default BasicContent;
