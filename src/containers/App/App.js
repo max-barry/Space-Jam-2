45fea18c2f82f3bc51903db277bb289f75eed936
Max Barry <max@mxbry.com>
2018-09-28 00:25:46 +0100

    App now has a router as part of it

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import styled from 'react-emotion';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Home from '../Home';
 import stars from '../../images/bg_stars.png';
+import BasicContent from '../BasicContent';
 
 const Container = styled('main')({
     backgroundColor: 'black',
-    backgroundImage: `url(${stars})`,
-    backgroundSize: '9%'
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+    minHeight: '100vh',
+    minWidth: '100vw',
+    '&.lazyloaded::before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+        backgroundImage: `url(${stars})`,
+        backgroundSize: '9%',
+        pointerEvents: 'none'
+    }
 });
 
 class App extends Component {
     render = () => (
-        <Container>
-            <Home />
-        </Container>
+        <TransitionGroup style={{ position: 'relative' }}>
+            <CSSTransition
+                key={this.props.location.key}
+                timeout={620}
+                classNames="transition"
+            >
+                <Container className="lazyload">
+                    <Switch location={this.props.location}>
+                        <Route exact path="/" component={Home} />
+                        <Route
+                            exact
+                            path="/basic-content"
+                            component={BasicContent}
+                        />
+                    </Switch>
+                </Container>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
 
-export default App;
+export default withRouter(App);
